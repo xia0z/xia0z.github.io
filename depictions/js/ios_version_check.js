787bcfd987f13787851540d2729139ea89856e43
Thế Nam <74763776+ntnamk3@users.noreply.github.com>
2022-06-15 21:43:16 +0700

    Update ios_version_check.js

--- depictions/js/ios_version_check.js
+++ depictions/js/ios_version_check.js
@@ -47,10 +47,10 @@ Twitter - @TimonOlsthoorn
 
 
 // changed const to var for IE9/10 compatibity.
-var VERSION_CHECK_SUPPORTED = "Your iOS version is supported! &#x1f60a;";
-var VERSION_CHECK_NEEDS_UPGRADE = "Requires at least iOS %s &#x1f615;";
-var VERSION_CHECK_UNCONFIRMED = "Not yet tested on iOS %s &#x1f601;";
-var VERSION_CHECK_UNSUPPORTED = "Only compatible with iOS %s to %s &#x1f61e;";
+var VERSION_CHECK_SUPPORTED = "Phiên bản iOS của bạn được hỗ trợ! &#x1f60a;";
+var VERSION_CHECK_NEEDS_UPGRADE = "Yêu cầu ít nhất iOS %s &#x1f615;";
+var VERSION_CHECK_UNCONFIRMED = "Chưa được thử nghiệm trên iOS %s &#x1f601;";
+var VERSION_CHECK_UNSUPPORTED = "Chỉ tương thích với iOS %s to %s &#x1f61e;";
 
 function ios_version_check(minIOS,maxIOS,otherIOS,callBack) {
 	"use strict";
